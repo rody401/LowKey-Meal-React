702e499d1deb2132b5f14908886695043a9ed950
rody401 <rodygomes401@gmail.com>
2022-01-17 01:51:30 -0500

    introducing auth sign in and code clean up

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { Container, AppBar, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Posts from './components/Posts/Posts.js';
-import Form from './components/Form/Form.js';
-import { getPosts } from './actions/posts.js';
-import useStyles from './styles.js';
-import logo from './images/LowKeyMeal.png';
+import Navbar from './components/Navbar/Navbar.js';
+import Home from './components/Home/Home.js'
+import Auth from './components/Auth/Auth.js';
+
 
 const App = () => {
-  const [currentId, setCurrentId] = useState(0);
-  const dispatch = useDispatch();
-  const classes = useStyles();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
-
   return (
-    <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <img className={classes.image} src={logo} alt="icon" height="300" width="95%" />
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home/>}/>
+          <Route path="/auth" exact element={<Auth/>}/>
+        </Routes>
+      </Container>
+    </BrowserRouter>
+
   );
 };
 
